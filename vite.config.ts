818fe2f499b7332f1aab56e836ffe2d2a1194c32
nishvi <vnishavishal@gmail.com>
2026-02-03 16:17:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -8,6 +7,7 @@ export default defineConfig({
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    outDir: 'dist'
   }
 });
